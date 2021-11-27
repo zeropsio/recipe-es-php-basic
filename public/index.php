9ac174797b14bcb55240183e4bf1a445e373e7b4
Miloš Lapiš <mlapis@mlc.cz>
2021-11-27 14:28:30 +0100

    wip(recipe): ES client SDK

--- public/index.php
+++ public/index.php
@@ -7,55 +7,69 @@
 	// Import relevant SDK classes.
 	use Elasticsearch\ClientBuilder;
 
-	try {
-		// The chosen hostname of the Elasticsearch service.
-		$hostname = "recipees";
+	// Open the logging channel.
+	openlog("", LOG_PID, LOG_LOCAL0);
 
-		// Function returning an connectionString environment variable of the <$hostname> service.
-		function getConnectionString($hostname) {
-			// The requested environment variable name.
-			$connectionString = "connectionString";
-			$host = getenv("${hostname}_${connectionString}");
-			if ($host) {
-				return $host;
-			}
-			// If any environment variable not found, return only the null value.
-			return null;
+	// The chosen hostname of the Elasticsearch service.
+	$hostname = "recipees";
+
+	// Function returning an connectionString environment variable of the <$hostname> service.
+	function getConnectionString($hostname) {
+		// The requested environment variable name.
+		$connectionString = "connectionString";
+		$host = getenv("${hostname}_${connectionString}");
+		if ($host) {
+			return $host;
 		}
-
-		// Function returning an object of the Elasticsearch SDK client.
-		function getEsClient($hostname) {
-			// For example, the result of the <host> would be: ["http://recipees:9200"]
-			$host = getConnectionString($hostname);
-			if ($host) {
-				return ClientBuilder::create()
-					->setHosts([$host])
-					// Sniffing should be disabled.
-					->setSniffOnStart(false)
-					->build();
-			}
-			return null;
-		}
-
-		// Declaration of the Elasticsearch SDK API client.
-		$esClient = getEsClient($hostname);
-
-		// Function inserting a new document.
-		function insert($esClient) {
-			$insertResult = $esClient->index([
-				"index" => "zerops-recipes",
-				"body" => [
-					"service" => "PHP",
-					"version" => "8.0.3",
-					"message" => "es-php-basic"
-				]
-			]);
-			echo $insertResult;
-		}
-
-		getEsClient($hostname);
-
-	} catch (Exception $e) {
-		echo 'Error: ' . $e->getCode() . ':' . $e->getMessage();
+		// If any environment variable not found, return only the null value.
+		return null;
 	}
+
+	// Function returning an object of the Elasticsearch SDK client.
+	function getEsClient($hostname) {
+		// For example, the result of the <host> would be: ["http://recipees:9200"]
+		$host = getConnectionString($hostname);
+		if ($host) {
+			return ClientBuilder::create()
+				->setHosts([$host])
+				// Sniffing should be disabled.
+				->setSniffOnStart(false)
+				->build();
+		}
+		return null;
+	}
+
+	// Declaration of the Elasticsearch SDK API client.
+	$esClient = getEsClient($hostname);
+
+	// Function inserting a new document.
+	function insert($esClient) {
+		if ($esClient) {
+			try {
+				$insertResult = $esClient->index([
+					"index" => "zerops-recipes",
+					"body" => [
+						"service" => "PHP",
+						"version" => "8.0.3",
+						"message" => "es-php-basic"
+					]
+				]);
+				echo('... Hello! A new document was inserted into Elasticsearch!');
+				syslog(LOG_ERR, "... Hello! A new document was inserted into Elasticsearch!");
+				// .statusCode
+			} catch (Exception $e) {
+				echo "... Error! Elasticsearch insert operation failed: " . $e->getCode() . ':' . $e->getMessage();
+				syslog(LOG_ERR, "... Error! Elasticsearch insert operation failed: " . $e->getCode() . ':' . $e->getMessage());
+			}
+		} else {
+			echo "... Error! Elasticsearch SDK API client not initialized.";
+			syslog(LOG_ERR, "Error! Elasticsearch SDK API client not initialized.");
+		}
+	}
+
+	getEsClient($hostname);
+
+	// Close the logging channel.
+	closelog();
+
 ?>
