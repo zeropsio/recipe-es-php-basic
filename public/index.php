5ed9b7d93b4b193a9c87e19bb600dbc1dd3e52df
Miloš Lapiš <mlapis@mlc.cz>
2021-11-27 13:22:43 +0100

    wip(recipe): ES client SDK

--- public/index.php
+++ public/index.php
@@ -1,5 +1,45 @@
 <?php
 
-	print "Recipe";
+	date_default_timezone_set('Europe/Prague');
+	// Include the SDK using the Composer auto-loader.
+	require '../vendor/autoload.php';
 
+	// Import relevant SDK classes.
+	use Elasticsearch\ClientBuilder;
+
+	try {
+		// The chosen hostname of the Elasticsearch service.
+		$hostname = "recipees";
+
+		// Function returning an connectionString environment variable of the <$hostname> service.
+		function getConnectionString($hostname) {
+			// The requested environment variable name.
+			$connectionString = "connectionString";
+			$host = getenv("${hostname}_${connectionString}");
+			if ($host) {
+				return $host;
+			}
+			// If any environment variable not found, return only the null value.
+			return null;
+		}
+
+		function getEsClient($hostname) {
+			// For example, the result of the <host> would be: ["http://recipees:9200"]
+			$host = getConnectionString($hostname);
+			if ($host) {
+				return ClientBuilder::create()
+					->setHosts($hosts)
+					// Sniffing should be disabled.
+					->setSniffOnStart(false)
+					->build();
+			}
+			return null;
+		}
+
+		// Declaration of the Elasticsearch SDK API client.
+		$esClient = getEsClient($hostname);
+
+	} catch (Exception $e) {
+
+	}
 ?>
